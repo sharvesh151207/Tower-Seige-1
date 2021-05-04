6f8b790fff9609e60399211825ac6c1a56286211
sharvesh151207 <75982212+sharvesh151207@users.noreply.github.com>
2021-05-04 15:04:41 +0530

    Add files via upload
    this is the corrected one

---
+++ box.js
@@ -0,0 +1,34 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER); 
+    strokeWeight(4);
+    stroke("red");
+    fill("blue");
+    rect(0, 0, this.width, this.height);
+    if(this.body.speed > 3){
+      World.remove(world,this.body);
+      this.Visibility = this.Visibility - 5;
+      tint(255,this.Visibility);
+    } 
+    else{
+     this.Visibility = true;
+    } 
+    pop();
+  }
+};
